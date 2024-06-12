5ce1e50f1c4155f43e188c7fd1219bddbb7a9272
yemedogbe <jeanmarietg@yahoo.fr>
2024-06-12 03:10:45 +0200

    new version

--- version.tf
+++ version.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "4.61.0"
+        version = "4.60.0"
     }
   }
 }
 
 provider "aws" {
-region = "us-east-1" 
+region = "us-east-2" 
 }
